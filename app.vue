8a1d53ad06d7394522e715e4cc7d948dd736863f
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-08-29 19:08:40 +0100

    Regenerated with nuxt3

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <threenine-header></threenine-header>
+  </div>
+</template>
+<script>
+import ThreenineHeader from "./components/ThreenineHeader";
+export default {
+  components: {ThreenineHeader}
+}
+</script>
---
+++ components/ThreenineHeader.vue
@@ -0,0 +1,273 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <Popover class="relative bg-white">
+    <div class="absolute inset-0 shadow z-30 pointer-events-none" aria-hidden="true" />
+    <div class="relative z-20">
+      <div class="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
+        <div>
+          <a href="#" class="flex">
+            <span class="sr-only">Threenine</span>
+            <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=orange&shade=600" alt="" />
+          </a>
+        </div>
+        <div class="-mr-2 -my-2 md:hidden">
+          <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
+            <span class="sr-only">Open menu</span>
+            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+          </PopoverButton>
+        </div>
+        <div class="hidden md:flex-1 md:flex md:items-center md:justify-between">
+          <PopoverGroup as="nav" class="flex space-x-10">
+            <Popover v-slot="{ open }">
+              <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-500', 'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500']">
+                <span>Products</span>
+                <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />
+              </PopoverButton>
+
+              <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 -translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 -translate-y-1">
+                <PopoverPanel class="hidden md:block absolute z-10 top-full inset-x-0 transform shadow-lg bg-white">
+                  <div class="max-w-7xl mx-auto grid gap-y-6 px-4 py-6 sm:grid-cols-2 sm:gap-8 sm:px-6 sm:py-8 lg:grid-cols-4 lg:px-8 lg:py-12 xl:py-16">
+                    <a v-for="item in solutions" :key="item.name" :href="item.href" class="-m-3 p-3 flex flex-col justify-between rounded-lg hover:bg-gray-50">
+                      <div class="flex md:h-full lg:flex-col">
+                        <div class="flex-shrink-0">
+                          <span class="inline-flex items-center justify-center h-10 w-10 rounded-md bg-orange-500 text-white sm:h-12 sm:w-12">
+                            <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+                          </span>
+                        </div>
+                        <div class="ml-4 md:flex-1 md:flex md:flex-col md:justify-between lg:ml-0 lg:mt-4">
+                          <div>
+                            <p class="text-base font-medium text-gray-900">
+                              {{ item.name }}
+                            </p>
+                            <p class="mt-1 text-sm text-gray-500">
+                              {{ item.description }}
+                            </p>
+                          </div>
+                          <p class="mt-2 text-sm font-medium text-orange-600 lg:mt-4">Learn more <span aria-hidden="true">&rarr;</span></p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="bg-gray-50">
+                    <div class="max-w-7xl mx-auto space-y-6 px-4 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-6 lg:px-8">
+                      <div v-for="item in callsToAction" :key="item.name" class="flow-root">
+                        <a :href="item.href" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
+                          <component :is="item.icon" class="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
+                          <span class="ml-3">{{ item.name }}</span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </PopoverPanel>
+              </transition>
+            </Popover>
+            <Popover v-slot="{ open }">
+              <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-500', 'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500']">
+                <span>About</span>
+                <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />
+              </PopoverButton>
+
+              <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 -translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 -translate-y-1">
+                <PopoverPanel class="hidden md:block absolute z-10 top-full inset-x-0 transform shadow-lg">
+                  <div class="absolute inset-0 flex">
+                    <div class="bg-white w-1/2" />
+                    <div class="bg-gray-50 w-1/2" />
+                  </div>
+                  <div class="relative max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2">
+                    <nav class="grid gap-y-10 px-4 py-8 bg-white sm:grid-cols-2 sm:gap-x-8 sm:py-12 sm:px-6 lg:px-8 xl:pr-12">
+                      <div>
+                        <h3 class="text-base font-medium text-gray-500">Company</h3>
+                        <ul role="list" class="mt-5 space-y-6">
+                          <li v-for="item in company" :key="item.name" class="flow-root">
+                            <a :href="item.href" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
+                              <component :is="item.icon" class="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
+                              <span class="ml-4">{{ item.name }}</span>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div>
+                        <h3 class="text-base font-medium text-gray-500">Resources</h3>
+                        <ul role="list" class="mt-5 space-y-6">
+                          <li v-for="item in resources" :key="item.name" class="flow-root">
+                            <a :href="item.href" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
+                              <component :is="item.icon" class="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
+                              <span class="ml-4">{{ item.name }}</span>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </nav>
+                    <div class="bg-gray-50 px-4 py-8 sm:py-12 sm:px-6 lg:px-8 xl:pl-12">
+                      <div>
+                        <h3 class="text-base font-medium text-gray-500">From the blog</h3>
+                        <ul role="list" class="mt-6 space-y-6">
+                          <li v-for="post in blogPosts" :key="post.id" class="flow-root">
+                            <a :href="post.href" class="-m-3 p-3 flex rounded-lg hover:bg-gray-100">
+                              <div class="hidden sm:block flex-shrink-0">
+                                <img class="w-32 h-20 object-cover rounded-md" :src="post.imageUrl" alt="" />
+                              </div>
+                              <div class="w-0 flex-1 sm:ml-8">
+                                <h4 class="text-base font-medium text-gray-900 truncate">
+                                  {{ post.name }}
+                                </h4>
+                                <p class="mt-1 text-sm text-gray-500">
+                                  {{ post.preview }}
+                                </p>
+                              </div>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="mt-6 text-sm font-medium">
+                        <a href="#" class="text-orange-600 hover:text-orange-500"> View all posts <span aria-hidden="true">&rarr;</span></a>
+                      </div>
+                    </div>
+                  </div>
+                </PopoverPanel>
+              </transition>
+            </Popover>
+          </PopoverGroup>
+          <div class="flex items-center md:ml-12">
+            <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"> Sign in </a>
+            <a href="#" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-orange-600 hover:bg-orange-700"> Sign up </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <transition enter-active-class="duration-200 ease-out" enter-from-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
+      <PopoverPanel focus class="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+        <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+          <div class="pt-5 pb-6 px-5 sm:pb-8">
+            <div class="flex items-center justify-between">
+              <div>
+                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=orange&shade=600" alt="Workflow" />
+              </div>
+              <div class="-mr-2">
+                <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
+                  <span class="sr-only">Close menu</span>
+                  <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                </PopoverButton>
+              </div>
+            </div>
+            <div class="mt-6 sm:mt-8">
+              <nav>
+                <div class="grid gap-7 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-4">
+                  <a v-for="item in solutions" :key="item.name" :href="item.href" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
+                    <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-orange-500 text-white sm:h-12 sm:w-12">
+                      <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <div class="ml-4 text-base font-medium text-gray-900">
+                      {{ item.name }}
+                    </div>
+                  </a>
+                </div>
+                <div class="mt-8 text-base">
+                  <a href="#" class="font-medium text-orange-600 hover:text-orange-500"> View all products <span aria-hidden="true">&rarr;</span></a>
+                </div>
+              </nav>
+            </div>
+          </div>
+          <div class="py-6 px-5">
+            <div class="grid grid-cols-2 gap-4">
+
+              <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Company </a>
+
+              <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Resources </a>
+
+              <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Blog </a>
+
+              <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
+                +44 116 318 3635 </a>
+            </div>
+            <div class="mt-6">
+              <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-orange-600 hover:bg-orange-700"> Sign up </a>
+              <p class="mt-6 text-center text-base font-medium text-gray-500">
+                Existing customer?
+                {{ ' ' }}
+                <a href="#" class="text-orange-600 hover:text-orange-500"> Sign in </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </PopoverPanel>
+    </transition>
+  </Popover>
+</template>
+
+<script setup>
+import { Popover, PopoverButton, PopoverGroup, PopoverPanel } from '@headlessui/vue'
+import {
+  Bars3Icon,
+  BookmarkSquareIcon,
+  BriefcaseIcon,
+  BuildingOfficeIcon,
+  CommandLineIcon,
+  CheckCircleIcon,
+  ComputerDesktopIcon,
+  CircleStackIcon,
+  GlobeAltIcon,
+  InformationCircleIcon,
+  NewspaperIcon,
+  PhoneIcon,
+  PlayIcon,
+  ShieldCheckIcon,
+  ShieldExclamationIcon,
+  UserGroupIcon,
+  XMarkIcon,
+} from '@heroicons/vue/24/outline/index.js'
+import { ChevronDownIcon } from '@heroicons/vue/20/solid/index.js'
+
+const solutions = [
+  {
+    name: 'API Template Pack',
+    description: "Develop dotnet based REST API's quickly  ",
+    href: 'https://www.apitemplatepack.com',
+    icon: CommandLineIcon,
+  },
+  {
+    name: 'Data',
+    description: 'A Microsoft Entity Framework Core plugin providing Unit of work and repository pattern ',
+    href: '#',
+    icon: CircleStackIcon,
+  },
+  { name: 'Stop Web Crawlers', description: "Your customers' data will be safe and secure.", href: 'https://wordpress.org/plugins/stop-web-crawlers/', icon: ShieldExclamationIcon }
+]
+const callsToAction = [
+  { name: 'Watch Demo', href: '#', icon: PlayIcon },
+  { name: 'View All Products', href: '#', icon: CheckCircleIcon },
+  { name: '+44 116 318 3635', href: 'tel:+44-116-318-3635', icon: PhoneIcon },
+]
+const company = [
+  { name: 'About', href: '#', icon: InformationCircleIcon },
+  { name: 'Customers', href: '#', icon: BuildingOfficeIcon },
+  { name: 'Press', href: '#', icon: NewspaperIcon },
+  { name: 'Careers', href: '#', icon: BriefcaseIcon },
+  { name: 'Privacy', href: '#', icon: ShieldCheckIcon },
+]
+const resources = [
+  { name: 'Community', href: '#', icon: UserGroupIcon },
+  { name: 'Partners', href: '#', icon: GlobeAltIcon },
+  { name: 'Guides', href: '#', icon: BookmarkSquareIcon },
+  { name: 'Webinars', href: '#', icon: ComputerDesktopIcon },
+]
+const blogPosts = [
+  {
+    id: 1,
+    name: 'Boost your conversion rate',
+    href: '#',
+    preview: 'Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.',
+    imageUrl:
+        'https://images.unsplash.com/photo-1558478551-1a378f63328e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2849&q=80',
+  },
+  {
+    id: 2,
+    name: 'How to use search engine optimization to drive traffic to your site',
+    href: '#',
+    preview: 'Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.',
+    imageUrl:
+        'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2300&q=80',
+  },
+]
+</script>
