260f8a2f6d318f6a4e4b39ce5e0ba9be3c13df13
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-08-31 21:06:37 +0100

    added typography classes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,8 @@
 import { defineNuxtConfig } from 'nuxt'
 
 // @ts-ignore
-import tailwindTypography from '@tailwindcss/typography'
+import tailwindTypography  from '@tailwindcss/typography'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-// @ts-ignore
-// @ts-ignore
-// @ts-ignore
 export default defineNuxtConfig({
     app: {
         head: {
