46ada4614b95a70d3d9a4399f091d63eb1cca1e5
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-01-08 18:28:58 +0000

    fixed product microbe to get values from CMS

--- components/facets/footer/microbes/Products.vue
+++ components/facets/footer/microbes/Products.vue
@@ -2,17 +2,16 @@
   <div>
     <h3 class="text-base font-medium text-white">Products</h3>
     <ul role="list" class="mt-4 space-y-4">
-      <li v-for="item in navigation.products" :key="item.name">
-        <nuxt-link :to="item.href" class="text-base text-gray-300 hover:text-white">{{ item.name }}</nuxt-link>
+      <li v-for="item in products" :key="item.title">
+        <nuxt-link :to="item._path" class="text-base text-gray-300 hover:text-white">{{ item.title }}</nuxt-link>
       </li>
     </ul>
   </div>
 </template>
 <script lang="ts" setup>
-const navigation = {
-  products: [
-    { name: 'API Template Pack', href: '../products/apitemplatepack' },
 
+const products = await queryContent("products")
+    .sort({ _file: -1, $numeric: true })
+    .find();
 
-  ]}
 </script>
