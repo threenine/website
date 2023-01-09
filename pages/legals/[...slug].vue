428133f9d0d3d47a90b5329a829279a4c2a5e755
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-01-09 18:24:16 +0000

    fixed product microbe to get values from CMS

--- pages/legals/[...slug].vue
+++ pages/legals/[...slug].vue
@@ -4,7 +4,7 @@ import banner from '../../components/legal/banner'
 
 <template>
   <ContentDoc v-slot="{ doc }">
-    <banner :version="doc.version" :heading="doc.heading"  :image="doc.bannerImage" :version-date="doc.date"  :summary="doc.summary"  />
+    <banner :version="doc.version" :heading="doc.heading"  :image="doc.bannerImage" :version-date="doc.versionDate"  :summary="doc.summary"  />
 
     <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
       <div class="pt-12 sm:pt-16 lg:pt-20 max-w-prose lg:max-w-none text-gray-500 lg:col-start-1
