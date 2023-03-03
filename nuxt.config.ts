fa8cd762b103e334f7509d0977e9322343b1cbd3
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-03-03 20:12:19 +0000

    Removed inefficient configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,20 +19,10 @@ export default defineNuxtConfig({
     typescript: {
         strict: true
     },
-    tailwindcss: {
-        config: {
-            plugins: [ require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms')],
-            content: [
-                `components/**/*.{vue,js}`,
-                `layouts/**/*.vue`,
-                `pages/**/*.vue`,
-                `composables/**/*.{js,ts}`,
-                `plugins/**/*.{js,ts}`,
-                `App.{js,ts,vue}`,
-                `app.{js,ts,vue}`
-            ]
-        }
-    },
+        tailwindcss: {
+            cssPath: '~/assets/css/tailwind.css',
+            configPath: 'tailwind.config',
+        },
     vite: {
         plugins: [
             Icons({
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms')
+  ],
+}
