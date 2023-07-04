3ab27013d12b32e85f5f8ee7fc57b52018890476
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-07-04 21:30:48 +0100

    Added new Markdown components

---
+++ components/content/CallToAction.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+defineProps({
+  heading: {
+    type: String,
+    default: 'Default title'
+  },
+  text: {
+    type: String,
+    default: 'Default description'
+  },
+  image: {
+    type: String,
+    default: '/images/api-dev-cta.jpg'
+  }
+})
+
+</script>
+
+<template>
+  <section class="p-4 lg:p-8 bg-white">
+    <div class="container mx-auto space-y-12">
+      <div class="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
+        <img :src="image" alt="" class="h-80 dark:bg-gray-500 aspect-video">
+        <div class="flex flex-col justify-center flex-1 p-6 bg-white">
+          <h3 class="text-3xl font-bold">{{ heading}}</h3>
+          <p class="my-6 dark:text-gray-400">{{text }}</p>
+          <button type="button" class="bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded-full">Start your project</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
