5a001cef9376457f345f10f3adbaa1f5992b0827
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-07-17 13:00:21 +0100

    Contact Form configuration

--- services/SendEmail.ts
+++ services/SendEmail.ts
@@ -3,7 +3,7 @@ import emailjs from '@emailjs/browser';
 export async function send(values :any)  : Promise<Boolean> {
     const config = useRuntimeConfig();
 
-    console.log(values);
-    let result =  await emailjs.send(config.public.emailjs_api_service_id, config.public.emailjs_api_template_id, values.query, config.public.emailjs_api_public_key);
+
+    let result =  await emailjs.send(config.public.emailjs_api_service_id, config.public.emailjs_api_template_id, values, config.public.emailjs_api_public_key);
     return result.status === 200;
 }
