6d03511b4edc8f73f3d4620d1826806d4b018994
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-07-18 07:27:59 +0100

    Added validation error messages to contact form

--- components/contact-form.vue
+++ components/contact-form.vue
@@ -5,11 +5,11 @@ import {send} from "~/services/SendEmail";
 
 
 const schema = yup.object({
-  email: yup.string().required().email(),
-  firstName: yup.string().required().max(65),
-  lastName: yup.string().required().max(65),
-  phoneNumber: yup.string().required().max(65),
-  message: yup.string().required().max(1500),
+  email: yup.string().required('Valid email address is required').email('Provide a valid email address'),
+  firstName: yup.string().required('First name is required').max(65, 'First name must be less than 65 characters'),
+  lastName: yup.string().required('Lastname is required').max(65, 'Last name must be less than 65 characters'),
+  phoneNumber: yup.string().required('Phone number is required').max(18, 'Phone number must be less than 18 characters'),
+  message: yup.string().required('Provide a brief message').max(1500, 'Message must be less than 1500 characters'),
 });
 
 
@@ -29,8 +29,7 @@ async function onSubmit() {
 
     let result = await send(values);
     if (result) {
-
-     resetForm();
+       resetForm();
     }
 
   }
@@ -51,33 +50,36 @@ async function onSubmit() {
                    name="firstName" id="firstName"
                    autocomplete="given-name"
             />
+            <span class="text-red-500  text-xs">{{errors.firstName}}</span>
           </div>
         </div>
         <div>
           <label for="lastName" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
           <div class="mt-2.5">
             <input type="text" v-bind="lastName"  name="lastName" id="last_name" autocomplete="family-name" />
-          </div>
+            <span class="text-red-500 text-xs">{{errors.lastName}}</span>          </div>
         </div>
         <div class="sm:col-span-2">
           <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
           <div class="mt-2.5">
             <input type="email" v-bind="email" name="email" id="email"
                    autocomplete="email"
-
             />
+            <span class="text-red-500 text-xs">{{errors.email}}</span>
           </div>
         </div>
         <div class="sm:col-span-2">
           <label for="phoneNumber" class="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
           <div class="mt-2.5">
             <input type="tel" v-bind="phoneNumber"  name="phoneNumber" id="phone_number" autocomplete="tel"  />
+            <span class="text-red-500 text-xs ">{{errors.phoneNumber}}</span>
           </div>
         </div>
         <div class="sm:col-span-2">
           <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Message</label>
           <div class="mt-2.5">
             <textarea name="message" v-bind="message" id="message" rows=4 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6" />
+            <span class="text-red-500 text-xs">{{errors.message}}</span>
           </div>
         </div>
       </div>
