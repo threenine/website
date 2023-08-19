2388961fe9d070c2173e913f912898fe07bd12f6
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-08-19 21:29:23 +0100

    refactoring components

--- components/content/SkillCloud.vue
+++ components/content/SkillCloud.vue
@@ -1,27 +1,19 @@
 <script lang="ts" setup>
+const skills =[
+  {name: 'Bitcoin', icon: 'simple-icons:bitcoin'},
+  { name:'C# Microsoft .net', icon: 'mdi:language-csharp'},
+  {name: 'Rust', icon:'fa6-brands:rust' },
+  {name:'Digital Ocean', icon:'fa6-brands:digital-ocean'},
+  {name: 'Linux', icon: 'uil:linux'}
+]
 </script>
-
-
   <template>
-    <div class="bg-white">
+    <div>
       <div class="mx-auto max-w-7xl py-12 px-6 lg:px-8">
         <div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
-          <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
-            <Icon class="h-12 w-12 text-orange-500" name="simple-icons:bitcoin"  alt="Bitcoin" />
+          <div v-for="skill in skills" :key="skill.name" class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+            <Icon class="h-12 w-12 text-orange-500" :name="skill.icon"  :alt="skill.name" />
           </div>
-          <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <Icon name="mdi:language-csharp" class="h-12 w-12 text-orange-500" alt="C# Microsoft .net" />
-          </div>
-          <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <Icon name="fa6-brands:rust" class="h-12 w-12 text-orange-500" alt="Rust" />
-          </div>
-          <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <Icon class="h-12 w-12 text-orange-500" name="fa6-brands:digital-ocean" alt="Digital Ocean" />
-          </div>
-          <div class="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
-            <Icon class="h-12 w-12 text-orange-500"  name="uil:linux"  alt="Linux Specialists" />
-          </div>
-
         </div>
       </div>
     </div>
