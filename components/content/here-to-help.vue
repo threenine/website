60b1139dc9b112208d5461ad572f665ab987c2df
gary <garywoodfine@duck.com>
2023-08-22 21:57:55 +0100

    refreshing call to action

--- components/content/here-to-help.vue
+++ components/content/here-to-help.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="bg-orange-300 mt-5">
     <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
-      <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Need help starting your API project?<br /><br/>We'll help you with your API Design, Development <br/>and adopting the API First strategy.</h2>
+      <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Need help starting your API project?<br /><br/>We'll help you with your API First strategy, Design & Development</h2>
       <div class="mt-10 flex items-center gap-x-10 lg:mt-0 lg:flex-shrink-0">
         <a href="https://threenine.co.uk/contact" target="_blank" class="bg-orange-500 hover:bg-orange-400 text-gray-100 font-bold text-xl py-6 px-6 rounded inline-flex items-center">
           <Icon name="mdi:chat" class="fill-current w-8 h-8 mr-2" />
