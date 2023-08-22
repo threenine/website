df57b8a05127b007591600a53e97ca5fa30946f3
gary <garywoodfine@duck.com>
2023-08-22 18:11:13 +0100

    added call to action to test

--- components/content/here-to-help.vue
+++ components/content/here-to-help.vue
@@ -3,13 +3,10 @@
 <template>
   <div class="bg-orange-300 mt-5">
     <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
-      <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Need help starting your API project?<br />We'll help you with your API Design, Development <br/>and adopting the API First strategy.</h2>
+      <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Need help starting your API project?<br /><br/>We'll help you with your API Design, Development <br/>and adopting the API First strategy.</h2>
       <div class="mt-10 flex items-center gap-x-10 lg:mt-0 lg:flex-shrink-0">
         <a href="https://threenine.co.uk/contact" target="_blank" class="bg-orange-500 hover:bg-orange-400 text-gray-100 font-bold text-xl py-6 px-6 rounded inline-flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="fill-current w-4 h-4 mr-2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-          </svg>
-
+          <Icon name="mdi:chat" class="fill-current w-8 h-8 mr-2" />
           <span>Talk to us about APIs</span>
         </a>
 
